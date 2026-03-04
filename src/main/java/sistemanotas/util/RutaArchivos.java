d5a9d2d4e0c6090cd7dca191deca33527c022b1c
Alfredo Bautista <adbautista@unicesar.edu.co>
2026-03-03 22:03:13 -0500

    feat: crear paquete util y clase RutaArchivos

---
+++ src/main/java/sistemanotas/util/RutaArchivos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemanotas.util;
+
+/**
+ * Clase utilitaria que centraliza las rutas de los archivos .txt
+ * Si se cambia la ubicación, solo se modifica aquí.
+ */
+
+/**
+ *
+ * @author ALFREDOBAUTISTAROMER
+ */
+public class RutaArchivos {
+    public static final String ESTUDIANTES = "datos/estudiantes.txt";
+    public static final String DOCENTES    = "datos/docentes.txt";
+    public static final String ASIGNATURAS = "datos/asignaturas.txt";
+    public static final String NOTAS       = "datos/notas.txt";    
+}
